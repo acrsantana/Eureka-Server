49fca657b2b35c1bc6596b8335fb0e1ba89de160
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-12-10 22:15:26 -0300

    Initial Commit

---
+++ src/test/java/br/com/acrtech/planningpoker/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.acrtech.planningpoker.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class EurekaServerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
